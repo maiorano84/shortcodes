006f2d93edb4904c2f4a9141b2a9183dad534494
Matt Maiorano <maiorano84@hotmail.com>
2015-04-21 13:53:30 -0400

    Examples created

---
+++ examples/example.php
@@ -0,0 +1,15 @@
+<?php
+require '../vendor/autoload.php';
+
+$manager = new Maiorano\WPShortcodes\Manager\ShortcodeManager(array(
+    'ipsum'=>new Maiorano\WPShortcodes\Examples\Ipsum,
+    'age'=>new Maiorano\WPShortcodes\Examples\Age
+));
+
+$dateShortcode = new Maiorano\WPShortcodes\Shortcode\SimpleShortcode('date', null, function(){
+    return date('l \t\h\e jS \o\f F, Y');
+});
+//Supports method chaining on register/deregister
+echo $manager->register($dateShortcode)->doShortcode('Today is [date]').'<br><br>';
+//Supports specifying shortcodes
+echo $manager->doShortcode('I am [age units=minutes]September 19th, 1984[/age] old. "[date]" is not rendered', 'age').'<br><br>';
